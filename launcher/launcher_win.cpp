4d3066610c7bc46c5677f47e170d81502ac15dd3
sasha190409 <163661926+sasha190409@users.noreply.github.com>
2026-08-21 00:10:08 +0300

    Update launcher_win.cpp

--- launcher/launcher_win.cpp
+++ launcher/launcher_win.cpp
@@ -1,87 +1,147 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#include <dlfcn.h>
+#include <windows.h>
+#include <wchar.h>
+#include <array>
+#include <string>
 
-#if defined(__APPLE__)
-#include <CoreFoundation/CoreFoundation.h>
+#if !defined(DEDICATED)
+
+#define DLL_EXPORT extern "C" __declspec(dllexport)
+
+DLL_EXPORT DWORD NvOptimusEnablement = 1;
+DLL_EXPORT int AmdPowerXpressRequestHighPerformance = 1;
+
+DLL_EXPORT bool BSecureAllowed(unsigned char *, int, int)
+{
+    return true;
+}
+
+DLL_EXPORT int CountFilesCompletedTrustCheck()
+{
+    return 0;
+}
+
+DLL_EXPORT int CountFilesNeedTrustCheck()
+{
+    return 0;
+}
+
+DLL_EXPORT int GetTotalFilesLoaded()
+{
+    return 0;
+}
+
+DLL_EXPORT int RuntimeCheck(int, int)
+{
+    return 0;
+}
+
 #endif
 
 #if defined(DEDICATED)
 #define LAUNCHER_LIB "dedicated"
 #define SYMBOL_NAME "DedicatedMain"
+typedef int (*LauncherMain_t)(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd);
 #else
 #define LAUNCHER_LIB "launcher"
 #define SYMBOL_NAME "LauncherMain"
+typedef int (*LauncherMain_t)(bool bSecure, HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd);
 #endif
 
-typedef int (*LauncherMain_t)(int argc, char **argv);
 typedef void (*InstallGC_t)(bool dedicated);
 
-static void ErrorMessageBox(const char *format, ...)
+static void ErrorMessageBox(const wchar_t *format, ...)
 {
     va_list ap;
-    char message[4096];
+    wchar_t buffer[4096];
 
     va_start(ap, format);
-    vsnprintf(message, sizeof(message), format, ap);
+    _vsnwprintf_s(buffer, std::size(buffer), format, ap);
     va_end(ap);
 
-    fprintf(stderr, "csgo_gc: %s\n", message);
-
-#if defined(__APPLE__)
-    CFStringRef messageString = CFStringCreateWithCString(kCFAllocatorDefault, message, kCFStringEncodingUTF8);
-
-    CFOptionFlags responseFlags;
-
-    CFUserNotificationDisplayAlert(0,
-        kCFUserNotificationStopAlertLevel,
-        nullptr,
-        nullptr,
-        nullptr,
-        CFSTR("csgo_gc"),
-        messageString,
-        nullptr,
-        nullptr,
-        nullptr,
-        &responseFlags);
-
-    CFRelease(messageString);
-#else
-    fprintf(stderr, "csgo_gc: %s\n", message);
-#endif
+    MessageBoxW(nullptr, buffer, L"csgo_gc", MB_OK | MB_ICONERROR);
 }
 
-static void *LoadModuleAndFindSymbol(const char *modulePath, const char *symbol)
+static const wchar_t *LastErrorString()
 {
-    void *module = dlopen(modulePath, RTLD_NOW);
+    static wchar_t buffer[4096];
+
+    buffer[0] = '\0';
+
+    int error = GetLastError();
+
+    int result = FormatMessageW(
+        FORMAT_MESSAGE_FROM_SYSTEM
+            | FORMAT_MESSAGE_IGNORE_INSERTS
+            | FORMAT_MESSAGE_MAX_WIDTH_MASK,
+        nullptr,
+        error,
+        0,
+        buffer,
+        std::size(buffer),
+        nullptr);
+
+    if (!result)
+    {
+        _snwprintf_s(buffer, std::size(buffer), L"Unknown error (%d)", error);
+    }
+
+    return buffer;
+}
+
+static void *LoadModuleAndFindSymbol(const wchar_t *absoluteModulePath, const char *symbol)
+{
+    // Используем абсолютный путь и флаг, который заставляет искать зависимости
+    // в папке загружаемой библиотеки, не изменяя глобальный PATH.
+    HMODULE module = LoadLibraryExW(absoluteModulePath, nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
     if (!module)
     {
-        ErrorMessageBox("%s", dlerror());
+        ErrorMessageBox(L"Could not load '%s':\n%s", absoluteModulePath, LastErrorString());
         return nullptr;
     }
 
-    void *function = dlsym(module, symbol);
+    void *function = GetProcAddress(module, symbol);
     if (!function)
     {
-        ErrorMessageBox("%s", dlerror());
+        ErrorMessageBox(L"Could not find '%S' from '%s':\n%s", symbol, absoluteModulePath, LastErrorString());
         return nullptr;
     }
 
     return function;
 }
 
-int main(int argc, char **argv)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    const char *modulePath = "bin/" GC_LIB_DIR "/" LAUNCHER_LIB GC_LIB_SUFFIX GC_LIB_EXTENSION;
+    wchar_t baseDir[MAX_PATH];
+    wchar_t modulePath[MAX_PATH];
+
+    DWORD baseDirLength = GetModuleFileNameW(nullptr, baseDir, std::size(baseDir));
+    if (!baseDirLength || baseDirLength == std::size(baseDir))
+    {
+        ErrorMessageBox(L"GetModuleFileName failed:\n%ls", LastErrorString());
+        return 1;
+    }
+
+    // Отрезаем имя исполняемого файла
+    wchar_t *slash = wcsrchr(baseDir, '\\');
+    if (!slash)
+    {
+        slash = baseDir; // fallback
+    }
+    *slash = L'\0';
+
+    // НЕ модифицируем переменную PATH – это частая причина ложных срабатываний.
+    // Вместо этого полагаемся на LOAD_WITH_ALTERED_SEARCH_PATH в LoadLibraryExW.
+
+    _snwprintf_s(modulePath, std::size(modulePath), L"%ls\\bin\\" GC_LIB_DIR "\\" LAUNCHER_LIB GC_LIB_SUFFIX GC_LIB_EXTENSION, baseDir);
     LauncherMain_t LauncherMain = (LauncherMain_t)LoadModuleAndFindSymbol(modulePath, SYMBOL_NAME);
     if (!LauncherMain)
     {
         return 1;
     }
 
-    modulePath = "csgo_gc/" GC_LIB_DIR "/"
-                 "csgo_gc" GC_LIB_EXTENSION;
+    _snwprintf_s(modulePath, std::size(modulePath), L"%ls\\csgo_gc\\" GC_LIB_DIR "\\"
+                                                    "csgo_gc" GC_LIB_EXTENSION,
+        baseDir);
     InstallGC_t InstallGC = (InstallGC_t)LoadModuleAndFindSymbol(modulePath, "InstallGC");
     if (!InstallGC)
     {
@@ -94,5 +154,9 @@ int main(int argc, char **argv)
     InstallGC(false);
 #endif
 
-    return LauncherMain(argc, argv);
+#if defined(DEDICATED)
+    return LauncherMain(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+#else
+    return LauncherMain(true, hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+#endif
 }
