f6eb20e55ae8a6e1daee97a59f19b2f43f796593
sasha190409 <163661926+sasha190409@users.noreply.github.com>
2026-08-21 22:53:28 +0300

    Update networking_server.cpp

--- csgo_gc/networking_server.cpp
+++ csgo_gc/networking_server.cpp
@@ -1,135 +1,59 @@
-#include "stdafx.h"
-#include "networking_server.h"
-#include "gc_message.h"
-#include "gc_server.h"
+#pragma once
 
-NetworkingServer::NetworkingServer(ISteamNetworkingMessages *networkingMessages)
-    : m_networkingMessages{ networkingMessages }
-    , m_sessionRequest{ this, &NetworkingServer::OnSessionRequest }
-    , m_sessionFailed{ this, &NetworkingServer::OnSessionFailed }
+#include "networking_shared.h"
+
+class GCMessageWrite;
+
+// this wrapper type is useless now, but i can't be bothered to inline it
+class ClientSet
 {
-}
+public:
+    bool Add(uint64_t id)
+    {
+        auto [it, inserted] = m_set.insert(id);
+        return inserted;
+    }
 
-bool NetworkingServer::ReceiveMessage(SteamNetworkingMessage_t *&message)
+    bool Remove(uint64_t id)
+    {
+        return (m_set.erase(id) == 1);
+    }
+
+    bool Has(uint64_t id) const
+    {
+        return (m_set.find(id) != m_set.end());
+    }
+
+private:
+    std::unordered_set<uint64_t> m_set;
+};
+class ServerGC;
+class NetworkingServer
 {
-    if (!m_networkingMessages->ReceiveMessagesOnChannel(NetMessageChannel, &message, 1))
-    {
-        return false;
-    }
+public:
+    NetworkingServer(ISteamNetworkingMessages *networkingMessages);
+    void SetServerGC(ServerGC* gc) { m_serverGC = gc; }
+    // caller need to call message->Release() because fuck you
+    bool ReceiveMessage(SteamNetworkingMessage_t *&message);
 
-    uint64_t steamId = message->m_identityPeer.GetSteamID64();
+    void ClientConnected(uint64_t steamId, const void *ticket, uint32_t ticketSize);
+    void ClientDisconnected(uint64_t steamId);
 
-    // see if we have a session
-    if (!m_clients.Has(steamId))
-    {
-        Platform::Print("NetworkingServer: ignored message from %llu (no session)\n", steamId);
-        message->Release();
-        return false;
-    }
+    void SendMessage(uint64_t steamId, const void *data, uint32_t size);
+    bool HasClient(uint64_t steamId) const { return m_clients.Has(steamId); }
 
-    return true;
-}
+private:
+    ServerGC* m_serverGC = nullptr;
+    ISteamNetworkingMessages *const m_networkingMessages;
+    ClientSet m_clients;
 
-// helper for SteamNetworkingMessages::SendMessageToUser that attempts to do some kind of error handling
-static void SendMessageToUser(ISteamNetworkingMessages *networkingMessages, uint64_t steamId, const void *data, uint32_t size)
-{
-    SteamNetworkingIdentity identity;
-    identity.SetSteamID64(steamId);
+    STEAM_GAMESERVER_CALLBACK(NetworkingServer,
+        OnSessionRequest,
+        SteamNetworkingMessagesSessionRequest_t,
+        m_sessionRequest);
 
-    EResult result = networkingMessages->SendMessageToUser(
-        identity,
-        data,
-        size,
-        NetMessageSendFlags,
-        NetMessageChannel);
-
-    if (result != k_EResultOK)
-    {
-        Platform::Print("SendMessageToUser failed for %llu: %d, closing session and trying again\n", steamId, result);
-
-        networkingMessages->CloseChannelWithUser(identity, NetMessageChannel);
-
-        result = networkingMessages->SendMessageToUser(
-            identity,
-            data,
-            size,
-            NetMessageSendFlags,
-            NetMessageChannel);
-
-        if (result != k_EResultOK)
-        {
-            // not much we can do in this situation i guess
-            Platform::Print("SendMessageToUser failed for %llu\n", steamId);
-        }
-    }
-}
-
-void NetworkingServer::ClientConnected(uint64_t steamId, const void *ticket, uint32_t ticketSize)
-{
-    if (!m_clients.Add(steamId))
-    {
-        Platform::Print("got ClientConnected for %llu but they're already on the list! ignoring\n", steamId);
-        return;
-    }
-
-    // send a message, if the client has csgo_gc installed they will
-    // reply with their so cache and we'll add them to our list
-    GCMessageWrite messageWrite{ k_EMsgNetworkConnect };
-    messageWrite.WriteUint32(ticketSize);
-    messageWrite.WriteData(ticket, ticketSize);
-    if (m_serverGC) {
-        m_serverGC->CheckPendingReservations();
-    }
-    // FIXME: this gets sent when the client is connecting to the server, it's not uncommon for
-    // the connection to time out, in which case the player's socache never gets to the server
-    SendMessageToUser(m_networkingMessages, steamId, messageWrite.Data(), messageWrite.Size());
-}
-
-void NetworkingServer::ClientDisconnected(uint64_t steamId)
-{
-    if (!m_clients.Remove(steamId))
-    {
-        Platform::Print("got ClientDisconnected for %llu but they're not on the list! ignoring\n", steamId);
-        return;
-    }
-
-    SteamNetworkingIdentity identity;
-    identity.SetSteamID64(steamId);
-    m_networkingMessages->CloseChannelWithUser(identity, NetMessageChannel);
-}
-
-void NetworkingServer::SendNetMessage(uint64_t steamId, const void *data, uint32_t size)
-{
-    if (!m_clients.Has(steamId))
-    {
-        Platform::Print("No csgo_gc session with %llu, not sending message!!!\n");
-        return;
-    }
-
-    SendMessageToUser(m_networkingMessages, steamId, data, size);
-}
-
-void NetworkingServer::OnSessionRequest(SteamNetworkingMessagesSessionRequest_t *param)
-{
-    uint64_t steamId = param->m_identityRemote.GetSteamID64();
-
-    if (!m_clients.Has(steamId))
-    {
-        Platform::Print("%llu sent a session request, we don't have a csgo_gc session, ignoring...\n");
-        return;
-    }
-
-    Platform::Print("%llu sent a session request, we were playing GC with them so accept\n");
-
-    if (!m_networkingMessages->AcceptSessionWithUser(param->m_identityRemote))
-    {
-        Platform::Print("AcceptSessionWithUser with %llu failed???\n",
-            param->m_identityRemote.GetSteamID64());
-    }
-}
-
-void NetworkingServer::OnSessionFailed(SteamNetworkingMessagesSessionFailed_t *param)
-{
-    // don't do anything, rely on the auth session
-    Platform::Print("OnSessionFailed: %s\n", param->m_info.m_szEndDebug);
-}
+    STEAM_GAMESERVER_CALLBACK(NetworkingServer,
+        OnSessionFailed,
+        SteamNetworkingMessagesSessionFailed_t,
+        m_sessionFailed);
+};
