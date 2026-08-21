d03c95d77f5dc0269c0ec881b4357b3775914a23
sasha190409 <163661926+sasha190409@users.noreply.github.com>
2026-08-21 22:16:03 +0300

    Update steam_hook.cpp

--- csgo_gc/steam_hook.cpp
+++ csgo_gc/steam_hook.cpp
@@ -29,33 +29,6 @@ struct SteamNetworkingIdentity;
 #include "networking_client.h"
 #include "networking_server.h"
 
-
-// =================================================================
-// Недостающие константы для анти-отладки (если не определены в winternl.h)
-// =================================================================
-#ifndef ProcessDebugPort
-#define ProcessDebugPort 7
-#endif
-#ifndef ProcessDebugObjectHandle
-#define ProcessDebugObjectHandle 30
-#endif
-#ifndef ProcessDebugFlags
-#define ProcessDebugFlags 31
-#endif
-#ifndef ThreadHideFromDebugger
-#define ThreadHideFromDebugger 17
-#endif
-#ifndef SystemKernelDebuggerInformation
-#define SystemKernelDebuggerInformation 35
-#endif
-#ifndef STATUS_SUCCESS
-#define STATUS_SUCCESS ((NTSTATUS)0)
-#endif
-#ifndef NT_SUCCESS
-#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
-#endif
-
-
 // =================================================================
 // Anti-Debug: обход всех стандартных проверок
 // =================================================================
@@ -142,10 +115,12 @@ NTSTATUS NTAPI Hook_NtSetInformationThread(
     PVOID ThreadInformation,
     ULONG ThreadInformationLength)
 {
+    // Если пытаются установить ThreadHideFromDebugger – ничего не делаем
     if (ThreadInformationClass == ThreadHideFromDebugger)
     {
         return STATUS_SUCCESS;
     }
+    // Иначе передаём дальше
     return Original_NtSetInformationThread(
         ThreadHandle,
         ThreadInformationClass,
@@ -181,15 +156,14 @@ NTSTATUS NTAPI Hook_NtQuerySystemInformation(
 static void PatchPEBBeingDebugged()
 {
 #ifdef _WIN64
-    // x64: PEB по адресу GS:[0x60]
+    // x64: PEB находится по адресу GS:[0x60]
+    // Используем встроенные intrinsic для чтения
     PPEB peb = (PPEB)__readgsqword(0x60);
-    if (peb)
-        peb->BeingDebugged = 0;
+    peb->BeingDebugged = 0;
 #else
     // x86: PEB по адресу FS:[0x30]
     PPEB peb = (PPEB)__readfsdword(0x30);
-    if (peb)
-        peb->BeingDebugged = 0;
+    peb->BeingDebugged = 0;
 #endif
 }
 
@@ -2150,7 +2124,7 @@ static void Hk_SteamAPI_RunCallbacks()
                 break;
 
             case HostEvent::NetMessage:
-                s_clientGC->m_networking.SendMessage(buffer.data(), static_cast<uint32_t>(buffer.size()));
+                (s_clientGC->m_networking.SendMessage)(buffer.data(), static_cast<uint32_t>(buffer.size())));
                 break;
 
             case HostEvent::MicroTransactionResponse:
